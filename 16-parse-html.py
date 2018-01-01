b3a0e73db21afb1db1197bb3016c4fe93d05cf7c
gimpei <gim.kobayashi@gmail.com>
2018-01-01 15:54:54 +0900

    a

--- 16-parse-html.py
+++ 16-parse-html.py
@@ -15,36 +15,40 @@ import concurrent.futures
 
 def _map(arr):
   key, names = arr
-  db = dbm.open("htmls/16-parsed_{key}.dbm".format(key=key), "c")
+  db = dbm.open("parsed_dbms/16-parsed_{key:09d}.dbm".format(key=key), "c")
   for name in names:
-    print(name)
-    soup = bs4.BeautifulSoup(open(name).read())
-    [s.extract() for s in soup('script')]
-    
-    title_parts = soup.find("div", {"class":"title-parts"})
-    if title_parts is None:
-      continue
+    try:
+      soup = bs4.BeautifulSoup(open(name).read(), "lxml")
+      [s.extract() for s in soup('script')]
+      
+      title_parts = soup.find("div", {"class":"title-parts"})
+      if title_parts is None:
+        continue
 
-    title = title_parts.find("h1")
-    subtitle = title_parts.find("h2")
+      title = title_parts.find("h1")
+      subtitle = title_parts.find("h2")
 
-    if title is None or subtitle is None:
-      os.remove(name)
-      continue
-    
-    date = soup.find("meta", property="article:published_time")
-    if date is None:
-      continue
-    print(date.get("content"))
-    body = soup.find("div", {"id":"article-body-inner"})
-    #print(name.replace("_","/"))
-    key = name.split("/").pop().replace("_","/")
-    val = { "title": title.text, 
-            "subtitle": subtitle.text,
-            "date": date.get("content"),
-            "body": body.text.strip() }
-    print(key,val)
-    db[ key ] = pickle.dumps(val)
+      if title is None or subtitle is None:
+        os.remove(name)
+        continue
+      
+      date = soup.find("meta", property="article:published_time")
+      if date is None:
+        continue
+      #print(date.get("content"))
+      body = soup.find("div", {"id":"article-body-inner"})
+      if body is None:
+        continue
+      #print(name.replace("_","/"))
+      key = name.split("/").pop().replace("_","/")
+      val = { "title": title.text, 
+              "subtitle": subtitle.text,
+              "date": date.get("content"),
+              "body": body.text.strip() }
+      #print(key,val)
+      db[ key ] = pickle.dumps(val)
+    except Exception as ex:
+      print(ex)
 
 arrs = {}
 for index, name in enumerate(glob.glob("htmls/*")):
