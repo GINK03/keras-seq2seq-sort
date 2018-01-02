94ea26640664e2bda42f938b4b9d022028031173
gimpei kobayashi <gim.kobayashi@gmail.com>
2018-01-03 04:26:37 +0900

    change cnn->dense

--- 24-train.py
+++ 24-train.py
@@ -31,19 +31,14 @@ def CBRD(inputs, filters=64, kernel_size=3, droprate=0.5):
 input_tensor = Input( shape=(10, 2000) )
 
 enc = input_tensor
-enc = CBRD(enc, 2)
-enc = CBRD(enc, 2)
-x = MaxPool1D()(enc)
-
-enc = CBRD(enc, 8)
-enc = CBRD(enc, 8)
-enc = MaxPool1D()(enc)
-
-encode = Flatten()(enc)
+enc = Flatten()(enc)
+enc = Dense(4024, activation="relu")( enc )
+encode = Dense(4024, activation="relu")( enc )
 
 dec = RepeatVector(30)(encode)
 dec = Bi(GRU(2647, return_sequences=True))(dec)
-decode  = TD(Dense(2647, activation='softmax'))(dec)
+dec = TD(Dense(2647, activation='relu'))(dec)
+decode  = TD(Dense(2647, activation='relu'))(dec)
 
 
 model = Model(inputs=input_tensor, outputs=decode)
@@ -55,6 +50,7 @@ import numpy as np
 import glob
 
 for i in range(500):
+  print(i)
   for name in glob.glob("dataset/*"):
     chunk = pickle.loads(gzip.decompress(open("dataset/000000000.pkl.gz", "rb").read()))
 
@@ -70,6 +66,7 @@ for i in range(500):
 
     X, y = np.array(X), np.array(y)
 
-    model.fit(X, y, epochs=1)
+    batch_size = random.randint(16, 64)
+    model.fit(X, y, epochs=1, batch_size=batch_size)
     #break
   model.save_weights("models/{:09d}.h5".format(i))
